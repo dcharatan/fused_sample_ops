7c706b7b31e743d1741d7bb1fbeed1ab5615bc00
David Charatan <david.charatan@gmail.com>
2023-08-23 16:11:17 -0400

    Figure out how to load module

--- src/extension.cpp
+++ src/extension.cpp
@@ -1,9 +1,16 @@
 #include <torch/extension.h>
 
+// Determine the module name based on the compilation method.
+#ifdef COMPILED_WITH_CMAKE
+#define MODULE_NAME libtorchdraw_cuda
+#else
+#define MODULE_NAME TORCH_EXTENSION_NAME
+#endif
+
 torch::Tensor render_lines(torch::Tensor samples) {
   return samples;
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(MODULE_NAME, m) {
   m.def("render_lines", &render_lines, "render lines (CUDA)");
 }
