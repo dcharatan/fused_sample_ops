c6516fc2aa296bc7de17105ffb7c37fc9e87342b
David Charatan <david.charatan@gmail.com>
2023-08-28 00:59:06 -0400

    Slight changes

--- src/conversions.cuh
+++ src/conversions.cuh
@@ -9,9 +9,9 @@ namespace torchdraw_cuda {
 template <typename scalar_t>
 __device__ glm::vec2 pixel_to_world(
     const glm::vec2 &xy,
-    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> bounds,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> &bounds,
     const torch::PackedTensorAccessor32<int64_t, 1, torch::RestrictPtrTraits>
-        image_shape) {
+        &image_shape) {
   const glm::vec2 minima = read_vec2(bounds[0]);
   const glm::vec2 maxima = read_vec2(bounds[1]);
   const glm::vec2 image_wh{static_cast<float>(image_shape[1]),
@@ -22,9 +22,9 @@ __device__ glm::vec2 pixel_to_world(
 template <typename scalar_t>
 __device__ glm::vec2 world_to_pixel(
     const glm::vec2 &xy,
-    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> bounds,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> &bounds,
     const torch::PackedTensorAccessor32<int64_t, 1, torch::RestrictPtrTraits>
-        image_shape) {
+        &image_shape) {
   const glm::vec2 minima = read_vec2(bounds[0]);
   const glm::vec2 maxima = read_vec2(bounds[1]);
   const glm::vec2 image_wh{static_cast<float>(image_shape[1]),
--- src/extension.cu
+++ src/extension.cu
@@ -9,7 +9,6 @@ torch::Tensor render_lines(torch::Tensor samples) {
 // TORCH_EXTENSION_NAME is defined by setup.py or CMakeLists.txt depending on
 // the compilation method.
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("render_points", &torchdraw_cuda::render_points, "render points (CUDA)");
   m.def("render_lines", &render_lines, "render lines (CUDA)");
-  m.def("render_points", &torchdraw_cuda::render_points,
-        "render points (CUDA)");
 }
