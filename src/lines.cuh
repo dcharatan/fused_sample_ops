18bb13a668deda76c7462f9b309ebdc3ec9dd727
David Charatan <david.charatan@gmail.com>
2023-08-28 01:55:33 -0400

    Port line kernel without testing it.

--- src/lines.cuh
+++ src/lines.cuh
@@ -7,6 +7,11 @@
 
 namespace torchdraw_cuda {
 
+// Define line cap types.
+constexpr int CAP_BUTT = 0;
+constexpr int CAP_ROUND = 1;
+constexpr int CAP_SQUARE = 2;
+
 torch::Tensor render_lines(torch::Tensor canvas,
                            torch::Tensor samples,
                            torch::Tensor starts,
@@ -33,27 +38,58 @@ __global__ void render_lines_kernel(
   if (sample_index < canvas.size(0)) {
     const glm::vec2 sample_xy = read_vec2(samples[sample_index]);
 
-    // // Iterate through the points.
-    // glm::vec3 color{0.f};
-    // bool hit = false;
-    // for (int point_index = 0; point_index < points.size(0); point_index++) {
-    //   // Compute the pixel-space distance between the point and the sample.
-    //   const glm::vec2 point_world_xy = read_vec2(points[point_index]);
-    //   const glm::vec2 point_pixel_xy =
-    //       world_to_pixel(point_world_xy, bounds, image_shape);
-    //   const float distance = glm::length(sample_xy - point_pixel_xy);
+    // Iterate through the lines.
+    glm::vec3 color{0.f};
+    bool hit = false;
+    for (int line_index = 0; line_index < starts.size(0); line_index++) {
+      // Compute pixel-space start and end coordinates.
+      const glm::vec2 start =
+          world_to_pixel(read_vec2(starts[line_index]), bounds, image_shape);
+      const glm::vec2 end =
+          world_to_pixel(read_vec2(ends[line_index]), bounds, image_shape);
 
-    //   // If the distance indicates a hit, update the color.
-    //   if (distance <= outer_radii[point_index] &&
-    //       distance >= inner_radii[point_index]) {
-    //     hit = true;
-    //     color = read_vec3(colors[point_index]);
-    //   }
-    // }
+      // Define a vector between the start and end points.
+      const glm::vec2 delta = end - start;
+      const float delta_norm = glm::length(delta);
+      const glm::vec2 u_delta = delta / delta_norm;
 
-    // // Write the resulting color.
-    // write_vec3(color, canvas[sample_index]);
-    // canvas[sample_index][3] = static_cast<float>(hit);
+      // Define a vector between the sample and the start point.
+      const glm::vec2 indicator = sample_xy - start;
+
+      // Determine whether the sample is inside the line in the parallel direction.
+      const int32_t cap = caps[sample_index];
+      const float width = widths[sample_index];
+      const float extra = (cap == CAP_SQUARE) ? 0.5f * width : 0.f;
+      const float parallel = glm::dot(u_delta, indicator);
+      const bool parallel_inside_line =
+          (parallel <= delta_norm + extra) && (parallel > -extra);
+
+      // Determine whether each sample is inside the line in the perpendicular
+      // direction.
+      const glm::vec2 perpendicular = indicator - parallel * u_delta;
+      const bool perpendicular_inside_line = glm::length(perpendicular) < 0.5f * width;
+
+      bool inside_line = parallel_inside_line && perpendicular_inside_line;
+
+      // Compute round caps.
+      if (cap == CAP_ROUND) {
+        const bool near_start = glm::length(indicator) < 0.5f * width;
+        inside_line |= near_start;
+        const glm::vec2 end_indicator = sample_xy - end;
+        const bool near_end = glm::length(end_indicator) < 0.5f * width;
+        inside_line |= near_end;
+      }
+
+      // If inside the line, update the color.
+      if (inside_line) {
+        hit = true;
+        color = read_vec3(colors[line_index]);
+      }
+    }
+
+    // Write the resulting color.
+    write_vec3(color, canvas[sample_index]);
+    canvas[sample_index][3] = static_cast<float>(hit);
   }
 }
 
