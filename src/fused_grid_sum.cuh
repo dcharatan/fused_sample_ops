7f1bf08c41ee5e120d69170d308f56ffea849d73
David Charatan <david.charatan@gmail.com>
2023-10-01 00:45:56 -0400

    untested forward pass

--- src/fused_grid_sum.cuh
+++ src/fused_grid_sum.cuh
@@ -25,69 +25,47 @@ __global__ void forward_kernel(
 
   const int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
   if (thread_index < num_threads) {
+    // Unravel the thread index.
     const int64_t i_b = (thread_index / result.stride(0)) % b;
     const int64_t i_hd = (thread_index / result.stride(1)) % hd;
     const int64_t i_s = (thread_index / result.stride(2)) % s;
     const int64_t i_c = (thread_index / result.stride(3)) % c;
-    result[i_b][i_hd][i_s][i_c] = 123.0;
+
+    // Sum over the sample_summed dimension.
+    const int64_t sample_summed = weights.size(3);
+    const int64_t height = image.size(2);
+    const int64_t width = image.size(3);
+    scalar_t sum = 0;
+    for (int32_t i = 0; i < sample_summed; i++) {
+      // Get X and Y. To match the convention in torch.nn.functional.grid_sample,
+      // we assume that each image axis is in range [-1, 1]. We convert to range [0, 1].
+      const scalar_t x = samples[i_b][i_s][i][0] * 0.5 + 0.5;
+      const scalar_t y = samples[i_b][i_s][i][1] * 0.5 + 0.5;
+      const int32_t col = static_cast<int32_t>(x * width);
+      const int32_t row = static_cast<int32_t>(y * height);
+
+      // Only add to the sum if the row and column are both valid. This is equivalent to
+      // grid sample with zero padding.
+      if (0 <= row && row < height && 0 <= col && col < width) {
+        const scalar_t row_fraction = fmod(y * height, 1.f);
+        const scalar_t col_fraction = fmod(x * width, 1.f);
+
+        const scalar_t top_left = image[i_b][i_c][row][col];
+        const scalar_t top_right = image[i_b][i_c][row][col + 1];
+        const scalar_t bottom_left = image[i_b][i_c][row + 1][col];
+        const scalar_t bottom_right = image[i_b][i_c][row + 1][col + 1];
+
+        // Run horizontal linear interpolation.
+        const scalar_t top = col_fraction * (1 - top_left) + col_fraction * top_right;
+        const scalar_t bottom =
+            col_fraction * (1 - bottom_left) + col_fraction * bottom_right;
+
+        // Run vertical linear interpolation.
+        sum += top * (1 - row_fraction) + bottom * row_fraction;
+      }
+    }
+    result[i_b][i_hd][i_s][i_c] = sum;
   }
-  // if (sample_index < canvas.size(0)) {
-  //   const glm::vec2 sample_xy = read_vec2(samples[sample_index]);
-
-  //   // Iterate through the lines.
-  //   glm::vec3 color{0.f};
-  //   bool hit = false;
-  //   for (int line_index = 0; line_index < starts.size(0); line_index++) {
-  //     // Compute pixel-space start and end coordinates.
-  //     const glm::vec2 start =
-  //         world_to_pixel(read_vec2(starts[line_index]), bounds, image_shape);
-  //     const glm::vec2 end =
-  //         world_to_pixel(read_vec2(ends[line_index]), bounds, image_shape);
-
-  //     // Define a vector between the start and end points.
-  //     const glm::vec2 delta = end - start;
-  //     const float delta_norm = glm::length(delta);
-  //     const glm::vec2 u_delta = delta / delta_norm;
-
-  //     // Define a vector between the sample and the start point.
-  //     const glm::vec2 indicator = sample_xy - start;
-
-  //     // Determine whether the sample is inside the line in the parallel direction.
-  //     const int32_t cap = caps[line_index];
-  //     const float width = widths[line_index];
-  //     const float extra = (cap == CAP_SQUARE) ? 0.5f * width : 0.f;
-  //     const float parallel = glm::dot(u_delta, indicator);
-  //     const bool parallel_inside_line =
-  //         (parallel <= delta_norm + extra) && (parallel > -extra);
-
-  //     // Determine whether each sample is inside the line in the perpendicular
-  //     // direction.
-  //     const glm::vec2 perpendicular = indicator - parallel * u_delta;
-  //     const bool perpendicular_inside_line = glm::length(perpendicular) < 0.5f *
-  //     width;
-
-  //     bool inside_line = parallel_inside_line && perpendicular_inside_line;
-
-  //     // Compute round caps.
-  //     if (cap == CAP_ROUND) {
-  //       const bool near_start = glm::length(indicator) < 0.5f * width;
-  //       inside_line |= near_start;
-  //       const glm::vec2 end_indicator = sample_xy - end;
-  //       const bool near_end = glm::length(end_indicator) < 0.5f * width;
-  //       inside_line |= near_end;
-  //     }
-
-  //     // If inside the line, update the color.
-  //     if (inside_line) {
-  //       hit = true;
-  //       color = read_vec3(colors[line_index]);
-  //     }
-  //   }
-
-  //   // Write the resulting color.
-  //   write_vec3(color, canvas[sample_index]);
-  //   canvas[sample_index][3] = static_cast<float>(hit);
-  // }
 }
 
 }  // namespace fused_grid_sum
