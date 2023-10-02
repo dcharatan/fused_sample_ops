073052f20293ae0429947408a87013ae88bf36e8
David Charatan <david.charatan@gmail.com>
2023-10-02 15:51:34 -0400

    Update test.

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+from time import time
+
 import torch
 from jaxtyping import install_import_hook
+from tqdm import trange
 
 with install_import_hook(
     ("src",),
@@ -27,15 +30,45 @@ if __name__ == "__main__":
     samples = 2.5 * rand((b, s, s2, 2)) - 1.25
     weights = rand((b, hd, s, s2))
 
-    images_expected = images.clone().requires_grad_(True)
-    weights_expected = weights.clone().requires_grad_(True)
-    result_expected = fused_grid_sum_torch(images_expected, samples, weights_expected)
-    result_expected.sum().backward()
+    forward_time = 0
+    backward_time = 0
+    for i in trange(100):
+        images_expected = images.clone().requires_grad_(True)
+        weights_expected = weights.clone().requires_grad_(True)
+        torch.cuda.synchronize()
+        start = time()
+        result_expected = fused_grid_sum_torch(
+            images_expected, samples, weights_expected
+        )
+        torch.cuda.synchronize()
+        forward_time += time() - start
 
-    images_actual = images.clone().requires_grad_(True)
-    weights_actual = weights.clone().requires_grad_(True)
-    result_actual = fused_grid_sum(images_actual, samples, weights_actual)
-    result_actual.sum().backward()
+        start = time()
+        result_expected.sum().backward()
+        torch.cuda.synchronize()
+        backward_time += time() - start
+
+    print(f"forward time: {forward_time}")
+    print(f"backward time: {backward_time}")
+
+    forward_time = 0
+    backward_time = 0
+    for i in trange(100):
+        images_actual = images.clone().requires_grad_(True)
+        weights_actual = weights.clone().requires_grad_(True)
+        torch.cuda.synchronize()
+        start = time()
+        result_actual = fused_grid_sum(images_actual, samples, weights_actual)
+        torch.cuda.synchronize()
+        forward_time += time() - start
+
+        start = time()
+        result_actual.sum().backward()
+        torch.cuda.synchronize()
+        backward_time += time() - start
+
+    print(f"forward time: {forward_time}")
+    print(f"backward time: {backward_time}")
 
     assert torch.allclose(result_actual, result_expected, atol=1e-4)
     print(f"result max diff: {(result_actual - result_expected).abs().max()}")
@@ -47,47 +80,3 @@ if __name__ == "__main__":
     print(
         f"weight grad max diff: {(weights_actual.grad - weights_expected.grad).abs().max()}"
     )
-
-    # stuff below this line is currently junk
-
-    for _ in trange(1):
-        torch.cuda.synchronize(device)
-        result = fused_grid_sum(image, samples, weights)
-        torch.cuda.synchronize(device)
-
-    for _ in trange(1):
-        torch.cuda.synchronize(device)
-        grid_samples = F.grid_sample(
-            image,
-            samples,
-            mode="bilinear",
-            padding_mode="zeros",
-            align_corners=False,
-        )
-        result2 = einsum(grid_samples, weights, "b c s s2, b hd s s2 -> b hd s c")
-        torch.cuda.synchronize(device)
-
-    def compare(samples):
-        image = torch.zeros((1, 1, 2, 2), dtype=torch.float32, device=device)
-        image[..., 0, 0] = 1
-        image[..., 0, 1] = 10
-        image[..., 1, 0] = 100
-        image[..., 1, 1] = 1000
-        samples = torch.tensor(samples, device=device, dtype=torch.float32)[
-            None, None, None
-        ]
-
-        original = F.grid_sample(
-            image,
-            samples,
-            mode="bilinear",
-            padding_mode="zeros",
-            align_corners=False,
-        )
-        ours = fused_grid_sum(
-            image, samples, torch.ones((1, 1, 1, 1), dtype=torch.float32, device=device)
-        )
-        return original, ours
-
-    a = 1
-    a = 1
