891db7478a201a3af74f8b59d03a8ba10a582eab
David Charatan <david.charatan@gmail.com>
2023-10-03 09:47:31 -0400

    Revert "change indexing but it doesn't really make a difference"
    This reverts commit 393bb913bc075f3845b870d73cb2d558a3ae2494.

--- src/sample_dot.cu
+++ src/sample_dot.cu
@@ -42,10 +42,9 @@ void fused_grid_sum::sample_dot_backward(torch::Tensor output_gradients,
   // We assume that 32-bit indexing can be used and that only float32 and float64 are
   // supported. We also assume that all tensors (except samples) need a gradient.
   int B = images.size(0);
-  int HD = queries.size(1);
   int Q = samples.size(1);
   int D = samples.size(2);
-  int num_threads = B * HD * Q * D;
+  int num_threads = B * Q * D;
   if (num_threads > 0) {
     AT_DISPATCH_FLOATING_TYPES(
         images.scalar_type(), "sample_dot_backward", ([&] {
